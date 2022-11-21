5fc07d4c930e17500722ec6472fcf83d13ecf923
Kushagra <kseth0808@gmail.com>
2022-11-21 21:59:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"))
+app.set('view engine', 'ejs');
+
+let text =[];
+app.get("/",function(req, res){
+    var date = new Date();
+    var options = {
+        weekday:"long",
+        day: "numeric",
+        month: "long"
+    }
+    var day = date.toLocaleDateString("en-US", options);
+    res.render("list", {listtitle: day, listitem: text})
+
+})
+app.post("/", function(req, res){
+    var texts = req.body.text;
+    text.push(texts)
+    res.redirect("/")
+}) 
+
+
+app.listen(process.env.PORT||3000, function(){
+    console.log("hello")
+})
